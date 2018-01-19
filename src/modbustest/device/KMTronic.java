2f187371b0cf8a7891fda015617f58cefba067c5
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2018-01-19 11:19:56 +0100

    Fix KMTronic printImportantValues implementation

--- src/modbustest/device/KMTronic.java
+++ src/modbustest/device/KMTronic.java
@@ -1,7 +1,5 @@
 package modbustest.device;
 
-import com.ghgande.j2mod.modbus.ModbusIOException;
-import com.ghgande.j2mod.modbus.ModbusSlaveException;
 import com.ghgande.j2mod.modbus.facade.ModbusSerialMaster;
 import com.ghgande.j2mod.modbus.util.BitVector;
 
@@ -16,17 +14,16 @@ public class KMTronic extends ModbusRtuDevice {
 		return "FEMS Relais (KMTronic)";
 	}
 
-	private String[] coilsStatus;
-
 	@Override
 	public boolean detectDevice() {
 		ModbusSerialMaster master = null;
 		try {
 			master = getModbusSerialMaster();
 			// if we are able to read exactly 8 coils, we believe this is a KMTronic board
-			master.readCoils(1, 0, 8);
+			master.readCoils(1, 0, 8);			
 			return true;
 		} catch (Exception e) {
+			System.out.println("  " + e.getMessage());
 			return false;
 		} finally {
 			if (master != null) {
@@ -37,34 +34,29 @@ public class KMTronic extends ModbusRtuDevice {
 
 	@Override
 	public void printImportantValues() {
-		ModbusSerialMaster master = new ModbusSerialMaster(getParameters());
-		for (int r = 0; r < 8; r++) {
-			coilsStatus = null;
-			try {
-				BitVector bits = master.readCoils(1, r, 1);
-				if (bits.getBit(0) == true) {
-					coilsStatus[r] = "true";
-					continue;
-				} else if (bits.getBit(0) == false) {
-					coilsStatus[r] = "false";
-					continue;
-				}
-			} catch (ModbusIOException ex) {
-				continue;
-			} catch (ModbusSlaveException ec) {
-				continue;
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-			}
-			System.out.println("KMTronic ------- Coil Status (Respectively 1 - to - 8) : " + coilsStatus[r]);
-		}
+		ModbusSerialMaster master = null;
+		try {
+			master = getModbusSerialMaster();
+			// if we are able to read exactly 8 coils, we believe this is a KMTronic board
+			BitVector coils = master.readCoils(1, 0, 8);
 
+			for (int i = 0; i < coils.size(); i++) {
+				boolean coil = coils.getBit(i);
+				System.out.print(i + 1 + " [" + coil + "]  ");
+			}
+			System.out.print("\n");
+		} catch (Exception e) {
+			System.out.println("  " + e.getMessage());
+		} finally {
+			if (master != null) {
+				master.disconnect();
+			}
+		}
 	}
 
 	@Override
 	public void printErrors() {
-		System.out.println(" ErrKM  ");
-
+		/* nothing to do */
 	}
 
 }
